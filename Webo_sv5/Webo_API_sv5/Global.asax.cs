d5672c6db971813c4510e3240d571d72982775a4
eyjolfur12 <ek@visir.is>
2013-03-28 13:34:37 -0400

    user modesl errors

--- Webo_sv5/Webo_API_sv5/Global.asax.cs
+++ Webo_sv5/Webo_API_sv5/Global.asax.cs
@@ -22,6 +22,7 @@ namespace Webo_API_sv5
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
             System.Data.Entity.Database.SetInitializer(new Webo_API_sv5.Models.ContextInitializer());
         }
     }
